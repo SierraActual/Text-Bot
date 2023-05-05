999a491e80deded225163b957afd8c85a535b2bc
chris <chrispitillo@gmail.com>
2023-05-05 20:02:40 +0100

    Added CSV reader and created functions

--- GUI_texter.py
+++ GUI_texter.py
@@ -1,35 +1,63 @@
 import pyautogui
 import time
+import csv
 
-# minimize terminal
-pyautogui.hotkey('alt', 'f9')
+def get_names_numbers():
+    # Open and read our CSV to store names and numbers
+    csv_file = 'path/to/your/file.csv'  #TODO Replace with the path to your CSV file
+    NAMES_AND_NUMBERS = [] 
 
-
-# Move the mouse to desktop file system and enter it
-pyautogui.moveTo(86, 81)
-pyautogui.click()
-pyautogui.click()
-
-# enter home folder
-pyautogui.moveTo(92, 196)
-pyautogui.click()
-
-# Open file tab
-pyautogui.moveTo(23, 70)
-pyautogui.click()
-
-# Hover over new doc and wait .5 secs
-pyautogui.moveTo(130, 196)
-time.sleep(.5)
+    #TODO edit to read NAMES and NUMBERS properly
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row:  # Skip empty rows
+                NAMES_AND_NUMBERS.append([row[0], row[1]])
+    
+    return NAMES_AND_NUMBERS
 
 
-pyautogui.moveTo(350, 288)
-pyautogui.click()
-time.sleep(.2)
+def send_text(name, number):
+    #TODO BELOW IS SAMPLE CODE. NEED TO CODE IN FULL WITH iMESSAGE
+    # minimize terminal
+    pyautogui.hotkey('alt', 'f9')
 
 
-# Type a string
-pyautogui.typewrite('new file durrr')
+    # Move the mouse to desktop file system and enter it
+    pyautogui.moveTo(86, 81)
+    pyautogui.click()
+    pyautogui.click()
 
-# Press the 'enter' key
-pyautogui.press('enter')
+    # enter home folder
+    pyautogui.moveTo(92, 196)
+    pyautogui.click()
+
+    # Open file tab
+    pyautogui.moveTo(23, 70)
+    pyautogui.click()
+
+    # Hover over new doc and wait .5 secs
+    pyautogui.moveTo(130, 196)
+    time.sleep(.5)
+
+
+    pyautogui.moveTo(350, 288)
+    pyautogui.click()
+    time.sleep(.2)
+
+
+    # Type a string
+    pyautogui.typewrite(f'new file {datetime.now().strftime("%H:%M:%S")}')
+
+    # Press the 'enter' key
+    pyautogui.press('enter')
+
+    # closefile browser
+    pyautogui.hotkey("alt", "f4")
+
+
+if __name__ == "__main__":
+    people = get_names_numbers()
+    for person in people:
+        send_text(person[0], person[1])
+
---
+++ tester_GUI_texter.py
@@ -0,0 +1,39 @@
+import pyautogui
+import time
+from datetime import datetime
+
+# minimize terminal
+pyautogui.hotkey('alt', 'f9')
+
+
+# Move the mouse to desktop file system and enter it
+pyautogui.moveTo(86, 81)
+pyautogui.click()
+pyautogui.click()
+
+# enter home folder
+pyautogui.moveTo(92, 196)
+pyautogui.click()
+
+# Open file tab
+pyautogui.moveTo(23, 70)
+pyautogui.click()
+
+# Hover over new doc and wait .5 secs
+pyautogui.moveTo(130, 196)
+time.sleep(.5)
+
+
+pyautogui.moveTo(350, 288)
+pyautogui.click()
+time.sleep(.2)
+
+
+# Type a string
+pyautogui.typewrite(f'new file {datetime.now().strftime("%H:%M:%S")}')
+
+# Press the 'enter' key
+pyautogui.press('enter')
+
+# closefile browser
+pyautogui.hotkey("alt", "f4")
